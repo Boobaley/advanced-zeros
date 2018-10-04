10a00f7d082fb000aee9d868b28e2ae095d4c20d
Adrian <38096991+Boobaley@users.noreply.github.com>
2018-10-04 10:57:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  const factorizedPrimes = primeFactorization(base);
+  const baseExponents = {};
+  factorizedPrimes.forEach(prime => {
+    const existingPrime = baseExponents[prime] || 0;
+    baseExponents[prime] = existingPrime + 1;
+  });
+
+  let result = 0;
+
+  for (let prime of Object.keys(baseExponents)) {
+    let devider = prime;
+    let tempZeroesCount = 0;
+    while (devider < number) {
+    tempZeroesCount += Math.floor(number / devider);
+    devider = devider * prime;
+    }
+    const tempResult = Math.floor(tempZeroesCount / baseExponents[prime]);
+    if (!result || result > tempResult) {
+    result = tempResult;
+    }
+  }
+  return result;
+}
+
+function primeFactorization(number, result) {
+  var result = (result || []);
+  var root = Math.sqrt(number);
+  var x = 2;
+
+  if (number % x) {
+    x = 3;
+
+    while ((number % x) && ((x = (x + 2)) < root)) {}
+  }
+
+  x = (x <= root) ? x : number;
+
+  result.push(x);
+
+  return (x === number) ? result : primeFactorization((number / x), result);
 }
